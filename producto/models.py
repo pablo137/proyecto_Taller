680247dd0ed2f40a5141342c8c0e5333072dee27
pablo137 <p48102611@gmail.com>
2023-05-14 16:20:55 -0400

    IMAGENES POR DEFAULT

--- producto/models.py
+++ producto/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Categorias(models.Model):
     nombre = models.CharField(max_length=50, unique=True, verbose_name='Nombre Categoria')
-    img = models.ImageField(default='categorias/img_defecto.jpg', upload_to='categorias/', verbose_name='Imagen de categoria')
+    img = models.ImageField(default='categorias/img_defecto_zstoxl.jpg', upload_to='categorias/', verbose_name='Imagen de categoria')
     
     class Meta:
         verbose_name = 'Categoria'
@@ -17,7 +17,7 @@ class Categorias(models.Model):
 class Productos(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='producto')
     nombre = models.CharField(max_length=150, null=False, blank=False, verbose_name='Nombre de producto')
-    img = models.ImageField(default='productos/img_defecto.jpg', upload_to='productos/', verbose_name='Imagen de producto')
+    img = models.ImageField(default='productos/img_defecto_cpzv2f.jpg', upload_to='productos/', verbose_name='Imagen de producto')
     categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE, verbose_name='Categorias', null=True, blank=True)
     descripcion = models.TextField(max_length=500, null=True)
     precio = models.DecimalField(max_digits=5, decimal_places=2)
