cd2baf0497b505494039cd6201d5d2b555db45d4
pablo137 <p48102611@gmail.com>
2023-05-14 16:56:41 -0400

    FIX SOME FIELDS

--- app_main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Emprendedor)
-admin.site.register(Consumidor)
-admin.site.register(Categorias)
-admin.site.register(Propietarios)
-admin.site.register(Producto)
