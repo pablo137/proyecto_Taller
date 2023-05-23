592ba99a52084933db96be2417a2f6e6cd054fb2
Fabio Copa Mejia <dahofcmmejia@gmail.com>
2023-05-23 00:57:59 -0400

    Ajustes a carrito

--- carrito/models.py
+++ carrito/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from producto.models import Productos
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Carrito(models.Model):
     producto_id = models.ForeignKey(Productos, on_delete=models.CASCADE, related_name='carrito')
-    
-    # nombre = producto_id.nombre
-    # precio = producto_id.precio
-    # img = producto_id.img
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usuario')
     nombre = models.CharField(max_length=150, null=False, blank=False, verbose_name='Nombre de producto')
     cantidad = models.PositiveIntegerField(default=1)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
